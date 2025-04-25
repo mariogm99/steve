4e8bebc42b10f0fc77dd0b60a85250875db36d1d
mariogm99 <mguraieb.ieu2020@student.ie.edu>
2025-04-25 13:54:31 +0200

    Removed copy files

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-# Replace with your actual Whoop API endpoint
-url = "https://api.prod.whoop.com/"
-
-response = requests.get(url)
-
-print("Status Code:", response.status_code)
-print("Response:", response.text)
